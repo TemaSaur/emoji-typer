d5df29573d195a064212522167ea14dbf67e1ede
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-10-13 23:52:45 +0500

    add event and json

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let emojis
+
+const loadEmojis = async () => {
+	const data = await fetch('./emojis.json')
+	const json = await data.json()
+	emojis = json
+}
+
+loadEmojis()
+
+const state = {
+	mode: false,
+	data: ""
+}
+
+const oninput = e => {
+	console.log(e)
+	if (!emojis)
+		return
+
+	if (e.data == " ") {
+		state.mode = false
+		state.data = ""
+		return
+	}
+
+	if (e.data == ":") {
+		state.mode = true
+		state.data = ""
+		return
+	}
+
+	if (e.data == null)
+
+	if (state.mode) {
+		state.data += e.data
+	}
+
+	if (state.mode)
+		console.log(e.data, state)
+}
+
 window.addEventListener("load", () => {
-	console.log("ready")
+	document.body.addEventListener("input", oninput)
 })
